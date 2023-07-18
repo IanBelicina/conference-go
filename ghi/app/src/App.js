bd155f370f00f7cf4f8489921658919fc462cd3b
Ian <vancebelicina@gmail.com>
2023-07-17 20:46:59 -0700

    Finished conference attendees form

--- ghi/app/src/App.js
+++ ghi/app/src/App.js
@@ -3,6 +3,7 @@ import Nav from './Nav';
 import AttendeesList from './AttendeesList';
 import LocationForm from './LocationForm';
 import ConferenceForm from './ConferenceForm';
+import AttendConferenceForm from './AttendConferenceForm'
 
 function App(props) {
   if (props.attendees === undefined) {
@@ -13,7 +14,8 @@ function App(props) {
       <Nav />
 
       <div className="container">
-        <ConferenceForm />
+        <AttendConferenceForm />
+        {/* <ConferenceForm /> */}
         {/* <LocationForm /> */}
         {/* <AttendeesList attendees={props.attendees}/> */}
       </div>
