8468cd0b09c53a2e57ca75db67c30cf8d6d517b1
Ian <vancebelicina@gmail.com>
2023-07-18 15:11:51 -0700

    finished new presentation form

--- ghi/app/src/App.js
+++ ghi/app/src/App.js
@@ -4,22 +4,30 @@ import AttendeesList from './AttendeesList';
 import LocationForm from './LocationForm';
 import ConferenceForm from './ConferenceForm';
 import AttendConferenceForm from './AttendConferenceForm'
+import PresentationForm from './PresentationForm';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 
 function App(props) {
   if (props.attendees === undefined) {
     return null;
   }
   return (
-    <React.Fragment>
-      <Nav />
 
+    <BrowserRouter>
+      <Nav />
       <div className="container">
-        <AttendConferenceForm />
-        {/* <ConferenceForm /> */}
-        {/* <LocationForm /> */}
-        {/* <AttendeesList attendees={props.attendees}/> */}
+        <Routes>
+          <Route path="attendees/new" element={<AttendConferenceForm />} />
+          <Route path="conferences/new" element={<ConferenceForm />} />
+          <Route path="locations/new" element={<LocationForm />} />
+          <Route path="attendees" element={<AttendeesList attendees={props.attendees} />}/>
+          <Route path="presentations/new" element={<PresentationForm />}/>
+        </Routes>
       </div>
-    </React.Fragment>
+    </BrowserRouter>
+
+
   );
 }
 export default App;
